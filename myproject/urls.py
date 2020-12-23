692b8d95d2f4709c40644c472d446a5f0e89e23b
Denis Rozhnov <drozhnov4925@bths.edu>
2020-12-23 12:58:10 -0500

    Protect views and can now reply to posts

--- myproject/urls.py
+++ myproject/urls.py
@@ -23,7 +23,7 @@ from accounts import views as accounts_views
 urlpatterns = [
     path('', views.home, name='home'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
-    path('boards/<int:pk>/new', views.new_topic, name='new_topic'),
+    path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
     path('signup/', accounts_views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
@@ -38,10 +38,13 @@ urlpatterns = [
         template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(
         template_name='password_reset_complete.html'), name='password_reset_complete'),
-    path(r'^settings/password/$', auth_views.PasswordChangeView.as_view(
+    path('settings/password/', auth_views.PasswordChangeView.as_view(
         template_name='password_change.html'), name='password_change'),
-    path(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(
+    path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(
         template_name='password_change_done.html'), name='password_change_done'),
+
+    path('boards/<int:pk>/topics/<int:topic_pk>/', views.topic_posts, name='topic_posts'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/reply/', views.reply_topic, name='reply_topic'),
 
     path('admin/', admin.site.urls),
 ]
