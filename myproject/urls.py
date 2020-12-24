46c52a9fd994c661adfa5c83fc27fb0bd34442b6
Denis Rozhnov <drozhnov4925@bths.edu>
2020-12-24 11:54:21 -0500

    Fixed change password

--- myproject/urls.py
+++ myproject/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
         subject_template_name=  'password_reset_subject.txt'), name='password_reset'),
     path('reset/done/', auth_views.PasswordResetDoneView.as_view(
         template_name='password_reset_done.html'), name='password_reset_done'),
-    path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view(
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
         template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(
         template_name='password_reset_complete.html'), name='password_reset_complete'),
